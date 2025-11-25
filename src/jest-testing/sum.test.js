7059dee16531671aad7222bf2086c4a3ddd483d6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-11-25 16:29:39 +0500

    Test Added: Add one more test to use toEqual matcher which checks the contents of array and object

--- src/jest-testing/sum.test.js
+++ src/jest-testing/sum.test.js
@@ -3,3 +3,13 @@ import {sum} from "./sum";
 test('1 + 2 equals to 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+});
+
+test('Object assignment', () => {
+    let data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one:1, two:2});
+});
